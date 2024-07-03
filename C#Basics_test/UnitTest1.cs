fc3e49cb502734a17ec6ce85ef0d1595fbd51149
Reem Hasan <23039864@student.ltuc.com>
2024-07-03 21:05:44 +0300

    solved cc7

--- C#Basics_test/UnitTest1.cs
+++ C#Basics_test/UnitTest1.cs
@@ -6,59 +6,91 @@ namespace C_Basics_test
     public class UnitTest1
     {
         //Testing when Arr2's length is bigger than Arr1's
-        [Fact]
-        public void big_arr2()
-        {
+        // [Fact]
+        //public void big_arr2()
+        //{
 
-            //Assign 
-            var arr1 = new int[] { 1, 2, 3,8 ,9,0,4};
-            var arr2 = new int[] { 45, 4, 3,8,9,22,22,22,22 };
-            var common = new int[] { 4,3,8,9};
-            //Act
-            C_BASICS.Program program= new C_BASICS.Program();
+        //    //Assign 
+        //    var arr1 = new int[] { 1, 2, 3,8 ,9,0,4};
+        //    var arr2 = new int[] { 45, 4, 3,8,9,22,22,22,22 };
+        //    var common = new int[] { 4,3,8,9};
+        //    //Act
+        //    C_BASICS.Program program= new C_BASICS.Program();
 
-            var result= program.CommonElements(arr1, arr2);
+        //    var result= program.CommonElements(arr1, arr2);
 
-            //Assert
-            Assert.Equal(result,common);
+        //    //Assert
+        //    Assert.Equal(result,common);
 
-        }
+        //}
 
         //Testing when Arr1's length is bigger than Arr2's
+        //[Fact]
+        //public void big_arr1()
+        //{
+
+        //    //Assign 
+        //    var arr1 = new int[] { 1, 2, 3, 4 };
+        //    var arr2 = new int[] { 45, 4};
+        //    var common = new int[] {4};
+        //    //Act
+        //    C_BASICS.Program program = new C_BASICS.Program();
+
+        //    var result = program.CommonElements(arr1, arr2);
+
+        //    //Assert
+        //    Assert.Equal(result, common);
+
+        //}
+
+        ////Testing when Arr1's length equals Arr2's
+        //[Fact]
+        //public void length_equal()
+        //{
+
+        //    //Assign 
+        //    var arr1 = new int[] { 1, 4 };
+        //    var arr2 = new int[] { 45, 4 };
+        //    var common = new int[] { 4 };
+        //    //Act
+        //    C_BASICS.Program program = new C_BASICS.Program();
+
+        //    var result = program.CommonElements(arr1, arr2);
+
+        //    //Assert
+        //    Assert.Equal(result, common);
+
+        //}
+
+
         [Fact]
-        public void big_arr1()
+        public void test_revers()
         {
-
             //Assign 
-            var arr1 = new int[] { 1, 2, 3, 4 };
-            var arr2 = new int[] { 45, 4};
-            var common = new int[] {4};
-            //Act
-            C_BASICS.Program program = new C_BASICS.Program();
+            //test1
+            string sent1 = "csharp is programming language";
+            string test1 = "language programming is csharp";
 
-            var result = program.CommonElements(arr1, arr2);
+            // test2
+
+            string sent2 = "Reverse the words in this sentence";
+            string test2 = "sentence this in words the Reverse";
+
+            // test3
+
+            string sent3 = "challenges and data structures";
+            string test3 = "structures data and challenges";
+
+            //Act
+            //Reverse_Words reverse_Words =new Reverse_Words();
+            string result1 = Reverse_Words.ReverseWords(sent1);
+            string result2 = Reverse_Words.ReverseWords(sent2);
+            string result3 = Reverse_Words.ReverseWords(sent3);
 
             //Assert
-            Assert.Equal(result, common);
-
-        }
-
-        //Testing when Arr1's length equals Arr2's
-        [Fact]
-        public void length_equal()
-        {
-
-            //Assign 
-            var arr1 = new int[] { 1, 4 };
-            var arr2 = new int[] { 45, 4 };
-            var common = new int[] { 4 };
-            //Act
-            C_BASICS.Program program = new C_BASICS.Program();
-
-            var result = program.CommonElements(arr1, arr2);
-
-            //Assert
-            Assert.Equal(result, common);
+            Assert.Equal(test1, result1);
+            Assert.Equal(test2, result2);
+            Assert.Equal(test3, result3);
 
         }
     }
