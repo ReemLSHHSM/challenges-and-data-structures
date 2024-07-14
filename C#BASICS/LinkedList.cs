312fbd99006ea827f07de7fab83a2c4242f456f5
Reem Hasan <23039864@student.ltuc.com>
2024-07-14 20:58:30 +0300

    done with whiteboard

--- C#BASICS/LinkedList.cs
+++ C#BASICS/LinkedList.cs
@@ -63,8 +63,9 @@ namespace C_BASICS
 
 
         //Display all elemints
-        public void Display()
+        public string Display()
         {
+            string eliments = "";
             Node node = Head;
             if (Head != null) {
 
@@ -72,6 +73,7 @@ namespace C_BASICS
                 {
 
                     Console.WriteLine(node.Data);
+                    eliments += node.Data+" ";
                     node = node.Next;
                 }
             }
@@ -79,6 +81,7 @@ namespace C_BASICS
             {
                 Console.WriteLine("Linked List is empty");
             }
+            return eliments;
         }
         public bool Includes(int data)
         {
@@ -94,30 +97,46 @@ namespace C_BASICS
             return false;
         }
 
-        public void RemoveDuplicate()
+        public string RemoveDuplicate()
         {
             Node current = Head;
 
-
-            while (current != null)
+            try
             {
-                Node pointer = current;
-                while (pointer.Next != null) {
 
-                    if (pointer.Next.Data == current.Data)
-                    {
-                        pointer.Next=pointer.Next.Next;
-                    }
-                    else
-                    {
-                        pointer=pointer.Next;
-                    }
 
+                while (current != null)
+                {
+                    Node pointer = current;
+                    while (pointer.Next != null)
+                    {
+
+                        if (pointer.Next.Data == current.Data)
+                        {
+                            pointer.Next = pointer.Next.Next;
+                        }
+                        else
+                        {
+                            pointer = pointer.Next;
+                        }
+
+                    }
+                    current = current.Next;
                 }
-                current=current.Next;
-            }
 
-            Display();
+              string eliments=  Display();
+                return eliments;
+
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("A null reference exception occurred: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error occurred: " + ex.Message);
+            }
+            return "";
         }
     }
 }
